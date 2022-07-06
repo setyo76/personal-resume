4ac5ccac7116ddbddf84e5635abdf2cf9ba40734
Agustinus Setyo Nugroho <setnug76@gmail.com>
2022-07-06 12:31:41 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# setyo-personalproject
+<br/>
+React template modification:
+<ul> 
+  <li>Nav bar</>
+    <li>Hero banner</>
+   <li>coloring</>
+    <li>button</>
+   <li>etc</>
+</ul>
+<br />
+<br />
+<p align="center">
+Preview page:
+<br />
+<br />
+<img width="250" alt="Preview Tugas HTML Profile Agustinus Setyo Nugroho" src="https://user-images.githubusercontent.com/88175067/172438174-eb9166c3-35fd-4a08-b9f1-6ac23e093858.png">
+</p>
+
---
+++ src/Components/Footer.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import Fade from "react-reveal";
+
+class Footer extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const networks = this.props.data.social.map(function (network) {
+      return (
+        <li key={network.name}>
+          <a href={network.url}>
+            <i className={network.className}></i>
+          </a>
+        </li>
+      );
+    });
+
+    return (
+      <footer>
+        <div className="row">
+          <Fade bottom>
+            <div className="twelve columns">
+              <ul className="social-links">{networks}</ul>
+
+              <ul className="copyright">
+                <li>&copy; Copyright 2022</li>
+              </ul>
+            </div>
+          </Fade>
+
+          <div id="go-top">
+            <a className="smoothscroll" title="Back to Top" href="#home">
+              <i className="icon-up-open"></i>
+            </a>
+          </div>
+        </div>
+      < div class="social">
+            <a href="https://wa.wizard.id/6886b0" target="_blank"><i class="fab fa-whatsapp"></i></a>
+            <a href="/"><i class="fab fa-facebook-f"></i></a>
+            <a href="/"><i class="fab fa-instagram"></i></a>
+    </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
